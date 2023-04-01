bcf5b003dc29626fbf08e775a0c4ef44870f7db3
DiegoLopezPaez <diegolopezpaez3464@gmail.com>
2023-04-01 19:27:55 -0300

    Creado el service para Transaction

--- src/main/java/com/alkemy/wallet/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/alkemy/wallet/service/impl/TransactionServiceImpl.java
@@ -1,32 +1,83 @@
 package com.alkemy.wallet.service.impl;
 
 import com.alkemy.wallet.dto.AccountDTO;
-import com.alkemy.wallet.dto.requestDto.TransactionDTORequest;
-import com.alkemy.wallet.repository.AccountRepository;
-import com.alkemy.wallet.service.service.AccountService;
+import com.alkemy.wallet.dto.requestDto.TransactionRequestDTO;
+import com.alkemy.wallet.dto.responseDto.UserModelResponseDTO;
+import com.alkemy.wallet.model.Currency;
+import com.alkemy.wallet.model.Transaction;
+import com.alkemy.wallet.model.Type;
+import com.alkemy.wallet.repository.TransactionRepository;
 import com.alkemy.wallet.service.service.TransactionService;
 import com.alkemy.wallet.service.service.UserModelService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Service
 public class TransactionServiceImpl implements TransactionService {
 
     @Autowired
-    private AccountService accountService;
+    private TransactionRepository transactionRepository;
+    @Autowired
+    private AccountServiceImpl accountService;
 
     @Autowired
     private UserModelService userModelService;
     @Override
-    public void sendUsd(TransactionDTORequest transactionRequestDTO, Long idSender) {
-        AccountDTO account = accountService.getAccount(idSender);
-        if(account.getUser().getUserId().equals(idSender)){
+    public void sendUsd(TransactionRequestDTO transactionRequestDTO, Long senderUserId) {
+        Long receiverUserId = transactionRequestDTO.getReceiverAccountId();
+        UserModelResponseDTO userSender = userModelService.getUserById(senderUserId);
+        UserModelResponseDTO userReceiver = userModelService.getUserById(receiverUserId);
+        if(userSender == null){
+            //TODO throw exception
         }
-        UserModelDTO userSender = userModelService.getUserById(idSender);
-        UserModelDTO userReceiver = userModelService.getUserById(account.getUser().getUserId());
+        if(userReceiver == null){
+            //TODO throw exception
+        }
+        if(receiverUserId.equals(senderUserId)){
+            //TODO throw exception
+        }
+        AccountDTO senderAccount = accountService.accountsOfUser(senderUserId)
+                .stream()
+                .filter(account -> account.getCurrency().name().equals(Currency.USD.name()))
+                .findAny().get();
+        AccountDTO receiverAccount = accountService.getAccountById(transactionRequestDTO.getReceiverAccountId());
 
+        generateTransaction(senderAccount, receiverAccount, transactionRequestDTO);
+    }
 
+    //TODO add throws
+    private void generateTransaction(AccountDTO senderAccount, AccountDTO receiverAccount, TransactionRequestDTO transactionRequestDTO){
+        checkTransaction(senderAccount, transactionRequestDTO);
+        StringBuilder description = new StringBuilder();
+        description.append("Se acredito ")
+                .append(transactionRequestDTO.getAmount())
+                .append(" a la cuenta ")
+                .append(receiverAccount.getUser().getFirstName())
+                .append(" ")
+                .append(receiverAccount.getUser().getLastName())
+                .append(" en la fecha ")
+                .append(LocalDate.now().toString());
 
+        Transaction newTransaction = Transaction.builder()
+                .type(Type.valueOf(transactionRequestDTO.getType()))
+                .account(accountService.getAccountEntityById(receiverAccount.getAccountId()))
+                .build();
+        newTransaction.setDescription(description.toString());
+
+        accountService.pay(receiverAccount.getAccountId(), transactionRequestDTO.getAmount());
+        accountService.discount(senderAccount.getAccountId(), transactionRequestDTO.getAmount());
+        transactionRepository.save(newTransaction);
+    }
+
+    //TODO add throws
+    private void checkTransaction(AccountDTO senderAccount, TransactionRequestDTO transactionRequestDTO){
+        if(senderAccount.getBalance() < transactionRequestDTO.getAmount()){
+            //TODO throw not enough money...
+        }
+        if(senderAccount.getTransactionLimit() < transactionRequestDTO.getAmount()){
+            //TODO throw amount surpass transfer limit
+        }
     }
 }
--- src/main/java/com/alkemy/wallet/service/service/TransactionService.java
+++ src/main/java/com/alkemy/wallet/service/service/TransactionService.java
@@ -1,10 +1,7 @@
 package com.alkemy.wallet.service.service;
 
-import com.alkemy.wallet.dto.requestDto.TransactionDTORequest;
-
-import javax.persistence.EnumType;
-import java.util.Enumeration;
+import com.alkemy.wallet.dto.requestDto.TransactionRequestDTO;
 
 public interface TransactionService {
-    void sendUsd(TransactionDTORequest transactionRequestDTO, Long idSender);
+    void sendUsd(TransactionRequestDTO transactionRequestDTO, Long userId);
 }
