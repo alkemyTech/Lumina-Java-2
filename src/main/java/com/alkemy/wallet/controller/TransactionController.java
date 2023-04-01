5b2f2567a812fa2236dd74f69be2fb1da817306b
DiegoLopezPaez <diegolopezpaez3464@gmail.com>
2023-04-01 19:23:22 -0300

    Agregado el controller para las transacciones, todavia falta completar la funcion sendUsd paar que devuelva el responseEntity con la transaccion

--- src/main/java/com/alkemy/wallet/controller/TransactionController.java
+++ src/main/java/com/alkemy/wallet/controller/TransactionController.java
@@ -1,7 +1,9 @@
 package com.alkemy.wallet.controller;
 
-import com.alkemy.wallet.dto.requestDto.TransactionDTORequest;
+import com.alkemy.wallet.dto.requestDto.TransactionRequestDTO;
+import com.alkemy.wallet.dto.responseDto.TransactionResponseDTO;
 import com.alkemy.wallet.service.service.TransactionService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,7 +13,8 @@ public class TransactionController {
     private TransactionService transactionService;
 
     @PostMapping("/sendUsd/{idSender}")
-    public void fun(@PathVariable Long idSender, @RequestBody TransactionDTORequest transactionRequestDTO){
+    public ResponseEntity<TransactionResponseDTO> sendUsd(@PathVariable Long idSender, @RequestBody TransactionRequestDTO transactionRequestDTO){
         transactionService.sendUsd(transactionRequestDTO, idSender);
+        return null;
     }
 }
