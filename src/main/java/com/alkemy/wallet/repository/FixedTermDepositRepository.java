2c7c6014d06f182566980d4948d0e004d91236ff
DiegoLopezPaez <diegolopezpaez3464@gmail.com>
2023-04-02 18:48:48 -0300

    Agregada la funcion para obtener la lista de plazos fijos segun un id de cuenta

--- src/main/java/com/alkemy/wallet/repository/FixedTermDepositRepository.java
+++ src/main/java/com/alkemy/wallet/repository/FixedTermDepositRepository.java
@@ -2,8 +2,15 @@ package com.alkemy.wallet.repository;
 
 import com.alkemy.wallet.model.FixedTermDeposit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface FixedTermDepositRepository extends JpaRepository<FixedTermDeposit, Long> {
+
+    @Query(value = "SELECT * FROM fixed_term_deposits WHERE accountId = :accountId", nativeQuery = true)
+    List<FixedTermDeposit> getFixedTermDepositDTObyAccountId(@Param(value = "accountID") Long accountId);
 }
