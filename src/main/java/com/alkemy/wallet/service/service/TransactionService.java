4cad2633883ccb8ac99a3fb5cef1c5d4cadd68d7
fornojp <fornojp@gmail.com>
2023-04-03 20:51:40 -0300

    Add Endpoint para envio de dinero en pesos se realiza endpoint para el envio de dinero en pesos, ticket LJT2-25

--- src/main/java/com/alkemy/wallet/service/service/TransactionService.java
+++ src/main/java/com/alkemy/wallet/service/service/TransactionService.java
@@ -6,4 +6,6 @@ import com.alkemy.wallet.model.UserModel;
 
 public interface TransactionService {
     TransactionResponseDTO sendUsd(TransactionRequestDTO transactionRequestDTO, Long userId) throws Exception;
+
+    TransactionResponseDTO sendArs(TransactionRequestDTO transactionRequestDTO, Long userId) throws Exception;
 }
