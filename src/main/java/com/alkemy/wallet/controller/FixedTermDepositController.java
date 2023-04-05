026d36c0dc06134cfff0f440ccf254c20bd9ea99
DiegoLopezPaez <diegolopezpaez3464@gmail.com>
2023-04-05 15:44:02 -0300

    Agregado el endpoint para la creacion de plazos fijos

---
+++ src/main/java/com/alkemy/wallet/controller/FixedTermDepositController.java
@@ -0,0 +1,25 @@
+package com.alkemy.wallet.controller;
+
+import com.alkemy.wallet.dto.requestDto.FixedTermDepositRequestDTO;
+import com.alkemy.wallet.service.service.FixedTermDepositService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/fixedDeposit")
+public class FixedTermDepositController {
+
+    @Autowired
+    FixedTermDepositService fixedTermDepositService;
+    @PostMapping("/{accountId}/{userId}")
+    public ResponseEntity<?> createFixedDeposit(@PathVariable Long accountId, @PathVariable Long userId, @RequestBody FixedTermDepositRequestDTO fixedTermDepositRequestDTO){
+        try{
+            return fixedTermDepositService.createFixedDeposit(accountId, userId, fixedTermDepositRequestDTO);
+        }
+        catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getStackTrace());
+        }
+    }
+}
