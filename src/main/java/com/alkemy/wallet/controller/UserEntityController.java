17b35d6ba425666c35a54b0aa49aaea17b8e678d
Jatzso <91919512+Jatzso@users.noreply.github.com>
2023-04-07 14:00:16 -0300

    Creación de método userRefreshValues

--- src/main/java/com/alkemy/wallet/controller/UserEntityController.java
+++ src/main/java/com/alkemy/wallet/controller/UserEntityController.java
@@ -1,10 +1,12 @@
 package com.alkemy.wallet.controller;
 
+import com.alkemy.wallet.Exception.UserNotFoundException;
 import com.alkemy.wallet.dto.requestDto.UserEntityRequestDTO;
 import com.alkemy.wallet.dto.responseDto.UserEntityResponseDTO;
 import com.alkemy.wallet.model.UserEntity;
 import com.alkemy.wallet.service.service.UserEntityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
